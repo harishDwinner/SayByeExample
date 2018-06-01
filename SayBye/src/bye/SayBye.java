714bd0117dd719fe32150f6d5686141960b96d9b
Harish <kommalapatiharish123@gmail.com>
2018-06-01 13:34:02 +0530

    FirstBye commit

---
+++ SayBye/src/bye/SayBye.java
@@ -0,0 +1,11 @@
+package bye;
+
+public class SayBye {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Say Bye....!");
+		System.out.println("See you later...!");
+	}
+
+}
